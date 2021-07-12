0cf3a9aa2653bb0ae23d2ccbc4fe958a51af9512
chris <cseiter@gmail.com>
2021-07-11 20:07:29 -0400

    total method passes test

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,14 @@
 class CashRegister
 
-    attr_accessor :items, :discount
+    attr_accessor :items, :discount, :total
 
     def initialize(discount=0)
         @total = 0
         @discount = discount
+    end
+
+    def total
+        @total
     end
 
 end
